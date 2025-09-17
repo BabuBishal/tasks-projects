ff2346a943bb290ddfeab71075cf99f06f6d7aca
BabuBishal <bohorabishal20@gmail.com>
2025-09-17 08:57:00 +0545

    counter using class component

--- react-1/src/App.jsx
+++ react-1/src/App.jsx
@@ -1,12 +1,14 @@
 import HelloWorld from "./components/HelloWorld";
 import Counter from "./components/Counter";
 import "./App.css";
+import CounterClass from "./components/CounterClass";
 
 function App() {
   return (
     <div className="app">
       <HelloWorld />
       <Counter />
+      <CounterClass />
     </div>
   );
 }
--- react-1/src/components/Counter.jsx
+++ react-1/src/components/Counter.jsx
@@ -3,10 +3,11 @@ import { useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
   return (
-    <div>
-      <h2>Counter: {count}</h2>
+    <div className="counter">
+      <h2>Counter using functional component: {count}</h2>
       <div className="buttons">
         <button onClick={() => setCount(count + 1)}>Increment</button>
+        {/* <button onClick={() => setCount(count + 1)}>Increment</button> */}
         <button onClick={() => setCount(count - 1)}>Decrement</button>
       </div>
     </div>
