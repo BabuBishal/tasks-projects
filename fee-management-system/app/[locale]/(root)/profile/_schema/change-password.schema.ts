406342a8a502801f1667da084b9683de79af0465
BabuBishal <bohorabishal20@gmail.com>
2025-12-05 15:35:50 +0545

    refactor: update schema type definition from JSONSchema7 to RJSFSchema and update pnpm lockfile.

--- fee-management-system/app/[locale]/(root)/profile/_schema/change-password.schema.ts
+++ fee-management-system/app/[locale]/(root)/profile/_schema/change-password.schema.ts
@@ -1,6 +1,6 @@
-import { JSONSchema7 } from 'json-schema'
+import { RJSFSchema } from '@rjsf/utils'
 
-const changePasswordSchema: JSONSchema7 = {
+const changePasswordSchema: RJSFSchema = {
   type: 'object',
   required: ['currentPassword', 'newPassword', 'confirmPassword'],
   properties: {
